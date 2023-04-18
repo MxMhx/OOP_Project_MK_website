40a8a8b1f1cf94df9af227fb24e1632759c1b220
Kasidit Samad <111700359+Ditokasiditz@users.noreply.github.com>
2023-04-18 14:04:21 +0700

    <feat>:add endpoint create order

--- backend/routers/cart.py
+++ backend/routers/cart.py
@@ -1,5 +1,8 @@
 from fastapi import APIRouter, HTTPException
 import sys
+from models.order import Order
+from models.product import Cart
+
 
 sys.path.append('/backend/')
 from data import mk
@@ -35,3 +38,12 @@ async def remove_cart_item(product_name: str, name: str):
     mk.get_user(name).get_cart().remove_cart_item(product_name)
 
     return mk.get_user(name).get_cart()
+
+
+@router.post("/create_order")
+async def create_order(order: Order):
+    cart_items = Cart.get_items()
+    total_price = sum(item.price for item in cart_items) + 50
+    order.total_price = total_price
+    order.status = "Pending"
+    return {"message": "Order created successfully!", "order_details": order.dict()}
