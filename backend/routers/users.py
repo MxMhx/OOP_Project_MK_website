718dfe6dfe243e7d9e89648ba9c7d877b947e969
Kasidit Samad <111700359+Ditokasiditz@users.noreply.github.com>
2023-05-01 11:18:27 +0700

    <fix>:add more user account

--- backend/routers/users.py
+++ backend/routers/users.py
@@ -10,7 +10,7 @@ router = APIRouter(prefix="/user", tags=['user'])
 
 
 mk.add_user(Customer("Ohm","0994448535",Account("ohm@gmail.com","123456"),"Japan"))
-mk.add_user(Admin("Tar","0945453453",Account("ohm@gmail.com","123456")))
+mk.add_user(Admin("Tar","0945453453",Account("tar@gmail.com","123456")))
 
 @router.get("/list")
 def read_user_list():
